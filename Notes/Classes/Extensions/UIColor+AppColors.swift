f819b52cf9093eeed0fcf0e28a00ddb2ca4637e0
Omkar khedekar <maxios.om@gmail.com>
2018-07-16 12:08:27 +0530

    Add app colors

---
+++ Notes/Classes/Extensions/UIColor+AppColors.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor+AppColors.swift
+//  Notes
+//
+//  Created by Omkar khedekar on 16/07/18.
+//  Copyright © 2018 Omkar khedekar. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static let appPurple = UIColor(named: "app_purple")
+    static let appBlue = UIColor(named: "app_blue")
+}
